9413df8596f53a927ff17c3f2bd2c03335c5b5f8
AhmedOsman21 <ahmedibrahimxzxzxz@gmail.com>
2022-06-03 05:45:05 +0200

    Removing fileExist class from routing as it was called by mistake

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use function PHPUnit\Framework\fileExists;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
